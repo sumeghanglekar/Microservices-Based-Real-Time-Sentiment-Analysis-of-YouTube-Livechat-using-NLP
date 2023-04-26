1eac34da4ca44ec02f3e3440e63ae2e7ae47400f
Sumegh Anglekar <sumeghanglekar98@gmail.com>
2023-04-26 11:37:13 -0400

    Prom yml changes, testing

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -7,6 +7,7 @@ from kafka import KafkaConsumer
 from json import loads
 from time import sleep
 import requests
+import json
 
 consumer = KafkaConsumer(
     'ytchats',
@@ -17,8 +18,10 @@ consumer = KafkaConsumer(
     value_deserializer=lambda x: loads(x.decode('utf-8'))
 )
 
+url = "http://localhost:8000/fastSentiment"
+
 # read csv from a github repo
-df = pd.read_csv("https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv")
+# df = pd.read_csv("https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv")
 
 st.set_page_config(
     page_title='Real-Time Data Science Dashboard',
@@ -45,44 +48,63 @@ while True:
     total_messages = 0
     positive_messages = 0
     negative_messages = 0
-
+    counts = {'total_messages':total_messages,
+              'positive_messages':positive_messages,
+              'negative_messages': negative_messages}
+    df = pd.DataFrame(counts,index=[0])
 
     newdf = pd.DataFrame(data, columns=['Numbers'])
 
     for event in consumer:
+        print(df)
         event_data = event.value
         data.append(event_data)
         print(data)
 
+        payload = json.dumps({
+            "msg": event_data
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        # response = requests.request("POST", url, headers=headers, data=payload)
+        # print(response.text)
+        #
+        # if response.text == 'positive':
+        #     positive_messages = positive_messages + 1
+        # elif response.text == 'negative':
+        #     negative_messages = negative_messages + 1
+        # else:
+        #     continue
+        df['positive_messages'] = df['positive_messages'] + 2
+        df['negative_messages'] = df['negative_messages'] + 1
         # creating KPIs
-        avg_age = np.mean(df['age_new'])
+        # avg_age = np.mean(df['age_new'])
 
-        count_married = int(df[(df["marital"] == 'married')]['marital'].count() + np.random.choice(range(1, 30)))
+        # count_married = int(df[(df["marital"] == 'married')]['marital'].count() + np.random.choice(range(1, 30)))
 
-        balance = np.mean(df['balance_new'])
+        # balance = np.mean(df['balance_new'])
 
         with placeholder.container():
             # create three columns
             kpi1, kpi2, kpi3 = st.columns(3)
 
             # fill in those three columns with respective metrics or KPIs
-            kpi1.metric(label="Total Messages ⏳", value=total_messages)
-            kpi2.metric(label="Married Count 💍", value=int(count_married), delta=- 10 + count_married)
-            kpi3.metric(label="A/C Balance ＄", value=f"$ {round(balance, 2)} ",
-                        delta=- round(balance / count_married) * 100)
+            kpi1.metric(label="Total Messages", value=df['total_messages'])
+            kpi2.metric(label="Positive Messages", value=df['positive_messages'])
+            kpi3.metric(label="Negative Messages", value=df['negative_messages'])
 
             # create two columns for charts
 
             fig_col1, fig_col2 = st.columns(2)
             with fig_col1:
-                st.markdown("### First Chart")
-                fig = px.density_heatmap(data_frame=df, y='age_new', x='marital')
-                st.write(fig)
-            with fig_col2:
-                st.markdown("### Second Chart")
-                fig2 = px.histogram(data_frame=df, x='age_new')
-                st.write(fig2)
-            st.markdown("### Detailed Data View")
-            st.dataframe(newdf)
-            time.sleep(1)
+                st.bar_chart(df)
+            # with fig_col2:
+            #     st.markdown("### Second Chart")
+            #     fig2 = px.histogram(data_frame=df, x='age_new')
+            #     st.write(fig2)
+            # st.markdown("### Detailed Data View")
+            st.dataframe(df)
+            time.sleep(10)
     # placeholder.empty()
