87a9be4d7b7d7661c42e40a9778cd8d8e1a5c33a
Sumegh Anglekar <sumeghanglekar98@gmail.com>
2023-05-06 21:25:08 -0400

    Time Elapsed Added in Sentiment Analyzer

--- sentiment-analyzer/main.py
+++ sentiment-analyzer/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from fasttext import load_model
+import time
 
 
 
@@ -11,17 +12,28 @@ def test():
 
 @app.route('/fastSentiment',methods=["POST"])
 def main():
+    start_time = time.time()
+
     classifier = load_model("model_tweet.bin")
     msg = request.json['msg']
     labels = classifier.predict(msg)
     print(labels)
     if(labels[0][0] == '__label__0'):
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("Elapsed time: ", elapsed_time)
         return 'negative'
 
     elif(labels[0][0] == '__label__4'):
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("Elapsed time: ", elapsed_time)
         return 'positive'
 
     else:
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print("Elapsed time: ", elapsed_time)
         return 'neutral'
 
 
